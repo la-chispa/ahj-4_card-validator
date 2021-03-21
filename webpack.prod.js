2bc394d3d54977c3f3f8184c3b4ce98304348161
Olga Belotserkovskaya <olla.chispa@gmail.com>
2021-03-21 17:20:59 +0600

    try to add license info

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-// const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
@@ -8,7 +8,18 @@ module.exports = merge(common, {
   optimization: {
     minimize: true,
     minimizer: [
-      `...`,
+      new TerserPlugin({
+        extractComments: {
+          condition: "some",
+          filename: (fileData) => {
+            // The "fileData" argument contains object with "filename", "basename", "query" and "hash"
+            return `${fileData.filename}.LICENSE.txt${fileData.query}`;
+          },
+          banner: (licenseFile) => {
+            return `License information can be found in ${licenseFile}`;
+          },
+        },
+      }),
       new CssMinimizerPlugin(),
     ],
   },
